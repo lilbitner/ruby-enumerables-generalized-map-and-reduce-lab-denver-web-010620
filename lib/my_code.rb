5157004aded47e23dd0ea70096cd9e53ea806776
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-06 22:02:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,45 +9,14 @@ def my_own_map(source_array)
   return new
 end
 
-def map_to_no_change(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] )
-    i += 1
-  end
-  return new
-end
+new.push( source_array[i] * -1 )
+new.push( source_array[i] )
+new.push( source_array[i] * 2 )
+new.push( source_array[i] * source_array[i] )
+new += source_array[i]
 
-def map_to_double(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * 2 )
-    i += 1
-  end
-  return new
-end
 
-def map_to_square(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * source_array[i] )
-    i += 1
-  end
-  return new
-end
 
-def reduce_to_total(source_array, starting_point=0)
-  new = starting_point
-  i = 0
-  while i < source_array.length do
-    new += source_array[i]
-    i += 1
-  end
-  return new
-end
 
 def reduce_to_all_true(source_array)
   i = 0
